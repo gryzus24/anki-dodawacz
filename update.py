73ff011a31fd434b19202a8748867b5b9731b84d
gryzus <gryzus24@gmail.com>
2021-12-08 21:50:55 +0100

    fix PermissionError when running `update.py` from the icon on Windows

--- update.py
+++ update.py
@@ -11,7 +11,7 @@ from requests.exceptions import Timeout
 
 from ankidodawacz import __version__
 from ankidodawacz import config, R, GEX, YEX, err_c
-from src.data import WINDOWS, LINUX
+from src.data import ROOT_DIR, WINDOWS, LINUX
 
 
 class Exit(Exception):
@@ -64,9 +64,10 @@ def main():
         print(f'{GEX.color}Korzystasz z najnowszej wersji ({__version__}).')
         return 0
 
-    new_dir_path = f'anki-dodawacz-{latest_tag["name"]}'
-    if os.path.exists(new_dir_path):
-        raise Exit(f'Folder o nazwie {new_dir_path} już istnieje.\n'
+    out_dir_name = f'anki-dodawacz-{latest_tag["name"]}'
+    out_dir_path = os.path.join(ROOT_DIR, out_dir_name)
+    if os.path.exists(out_dir_path):
+        raise Exit(f'Folder o nazwie {out_dir_name!r} już istnieje.\n'
                    f'Przerywam aktualizację...')
 
     if LINUX or WINDOWS:
@@ -86,13 +87,13 @@ def main():
         tfile.write(archive.content)
         tfile.flush()
         tfile.close()
-        os.mkdir(new_dir_path)
+        os.mkdir(out_dir_path)
         process = subprocess.run(
-            ['tar', '-xvf', tfile.name, '--strip-components=1', '-C', new_dir_path]
+            ['tar', '-xvf', tfile.name, '--strip-components=1', '-C', out_dir_path]
         )
         if process.returncode != 0:
             try:
-                os.rmdir(new_dir_path)
+                os.rmdir(out_dir_path)
             except OSError:  # In case tar gets interrupted,
                 pass         # otherwise the directory should be empty.
             raise Exit('Błąd podczas rozpakowywania archiwum.')
@@ -100,17 +101,17 @@ def main():
         os.remove(tfile.name)
 
     print(f"{GEX.color}:: {R}Przenoszę zawartość 'config.json'...")
-    with open(os.path.join(new_dir_path, 'config/config.json'), 'r') as f:
+    with open(os.path.join(out_dir_path, 'config/config.json'), 'r') as f:
         new_config = json.load(f)
         for key, val in config.items():
             new_config[key] = val
-    with open(os.path.join(new_dir_path, 'config/config.json'), 'w') as f:
+    with open(os.path.join(out_dir_path, 'config/config.json'), 'w') as f:
         json.dump(new_config, f, indent=0)
 
     if os.path.exists('karty.txt'):
         print(f"{GEX.color}:: {R}Przenoszę zawartość 'karty.txt'...")
         with \
-                open(os.path.join(new_dir_path, 'karty.txt'), 'w') as new_cards, \
+                open(os.path.join(out_dir_path, 'karty.txt'), 'w') as new_cards, \
                 open('karty.txt', 'r') as cards:
             new_cards.writelines(cards.readlines())
 
@@ -118,7 +119,7 @@ def main():
         print(f"{YEX.color}:: {R}Zawartość folderu 'Karty_audio' musi zostać przeniesiona manualnie.")
 
     print(f'\n{GEX.color}Aktualizacja zakończona pomyślnie.\n'
-          f'Program zapisany do folderu: {new_dir_path!r}')
+          f'Program zapisany do folderu: {out_dir_name!r}')
 
 
 if __name__ == '__main__':
