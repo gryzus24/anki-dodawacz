e2e72b8b5c6f45ada74099a7a50d3fbc428642b6
gryzus <gryzus24@gmail.com>
2023-07-20 16:09:20 +0200

    rewrite WordNet in lxml.etree

--- src/Dictionaries/wordnet.py
+++ src/Dictionaries/wordnet.py
@@ -6,36 +6,50 @@ from src.Dictionaries.base import HEADER
 from src.Dictionaries.base import LABEL
 from src.Dictionaries.base import PHRASE
 from src.Dictionaries.base import SYN
-from src.Dictionaries.util import request_soup
+from src.Dictionaries.util import parse_response
+from src.Dictionaries.util import prepare_check_text
+from src.Dictionaries.util import try_request
+
+DICTIONARY = 'WordNet'
 
 
 def ask_wordnet(query: str) -> Dictionary:
-    soup = request_soup('http://wordnetweb.princeton.edu/perl/webwn', {'s': query})
+    soup = parse_response(
+        try_request('http://wordnetweb.princeton.edu/perl/webwn', {'s': query})
+    )
 
-    header_tag = soup.find('h3')
-    if header_tag is None:
-        raise DictionaryError('WordNet: unexpected error, no header_tag')
+    h3_tag = soup.find('.//h3')
+    if h3_tag is None:
+        raise DictionaryError(f'ERROR: {DICTIONARY}: no header tag')
 
-    if header_tag.text.startswith(('Your', 'Sorry')):
-        raise DictionaryError(f'WordNet: {query!r} not found')
+    check_text = prepare_check_text(DICTIONARY)
+
+    h3_tag_text = check_text(h3_tag)
+    if h3_tag_text.startswith(('Your', 'Sorry')):
+        raise DictionaryError(f'{DICTIONARY}: {query!r} not found')
 
     wordnet = Dictionary()
 
-    wordnet.add(HEADER('WordNet'))
+    wordnet.add(HEADER(DICTIONARY))
     wordnet.add(PHRASE(query, ''))
-    wordnet.add(LABEL('', ''))
-    for t in soup.find_all('li'):
-        _, _, body = t.text.partition('(')
-        pos, _, body = body.partition(')')
-        syn, _, body = body.partition('(')
-        gloss, _, examples = body.rpartition(')')
+    wordnet.add(LABEL(h3_tag_text, ''))
+    for tag in h3_tag.itersiblings():
+        if tag.tag == 'h3':
+            wordnet.add(LABEL(check_text(tag), ''))
+        elif tag.tag == 'ul':
+            for li in tag.iterchildren('li'):
+                li_text = ''.join(li.itertext())  # type: ignore[arg-type]
+                _, _, body = li_text.partition('(')
+                pos, _, body = body.partition(')')
+                syn, _, body = body.partition('(')
+                gloss, _, examples = body.rpartition(')')
 
-        wordnet.add(
-            SYN(
-                syn.strip(),
-                f'({gloss.strip()})',
-                list(map(str.strip, examples.split(';')))
-            )
-        )
+                wordnet.add(
+                    SYN(
+                        syn.strip(),
+                        f'({gloss.strip()})',
+                        [x.strip() for x in examples.split(';')],
+                    )
+                )
 
     return wordnet
